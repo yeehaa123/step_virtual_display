4f4411caf9f87d00fa0132cfd470b27db3f460b8
Yeehaa <medialoperations@gmail.com>
2014-04-12 18:09:43 +0200

    initial commit

---
+++ run.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+export DISPLAY=:99.0
+start-stop-daemon --start --quiet --pidfile /tmp/xvfb_99.pid --make-pidfile --background --exec /usr/bin/Xvfb -- :99 -screen 0 1024x768x24 -ac +extension GLX +render -noreset
